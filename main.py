071c5b745f6bde57f514a5506caa220808704098
Miko Gronroos <miko.gronroos@gmail.com>
2025-11-30 15:20:38 +0000

    Modify the main.py to Flask server

--- main.py
+++ main.py
@@ -3,15 +3,8 @@ import Scripts.Core.authentication as auth
 import Scripts.Core.startNewGame as start
 import Scripts.Database.database as db
 import Scripts.Core.account as account
+from flask import Flask, request, jsonify
+app = Flask(__name__)
 
-import os
-
-#test
-
-os.system('cls' if os.name == 'nt' else 'clear')
-
-if auth.Authenticate():
-    if db.isNewPlayer(account.name):
-        start.startNewGame() 
-    while True:
-        gameLoop.GameLoop()
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
