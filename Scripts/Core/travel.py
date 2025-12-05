9483f8ada548ed2c18ceff6db78d83a5a6202923
Miko Gronroos <miko.gronroos@gmail.com>
2025-12-05 18:50:07 +0000

    Remove old unused code

--- Scripts/Core/travel.py
+++ Scripts/Core/travel.py
@@ -22,25 +22,3 @@ def move():
     
     return jsonify({"status": status})
 
-def travel1():
-    while True:
-        if db.HasMoney(account.name) == False:
-            print("You run out of money")
-        airports = db.takeAllAirports(db.getPlayerContinent(account.name))
-        print(f"You have {db.GetPlayerMoney(account.name)} coins")
-        print(f"A ticket costs 30 coins")
-        i = 0
-        for value in airports:
-            i = i + 1
-            print(f"{i}, name: {value[0]} and continent: {value[3]}")
-        selection = int(input("select airport 1-5 or exit 6: "))
-        if selection < 6 and selection > 0:
-            if db.CheckMoney(account.name, 30):
-                db.fly(airports[selection - 1][4], account.name)
-                db.UpdateMoney(account.name, -30)
-            else:
-                print("You do not have enough money to fly")
-                input("Press enter to continue...")
-        if selection == 6:
-            return
-
